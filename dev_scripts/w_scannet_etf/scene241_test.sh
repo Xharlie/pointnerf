c21658dcf019f3b3b0ff61f855ec3d8f5e74134d
xharlie <charlie.xu007@yahoo.com>
2022-02-12 20:21:10 -0800

    241

--- dev_scripts/w_scannet_etf/scene241_test.sh
+++ dev_scripts/w_scannet_etf/scene241_test.sh
@@ -7,36 +7,12 @@ name='scene241'
 resume_iter=200000 # 20000 #latest
 data_root="${nrDataRoot}/scannet/scans/"
 scan="scene0241_01"
-
-load_points=2
-feat_grad=1
-conf_grad=1
-dir_grad=1
-color_grad=1
-vox_res=900
 normview=0
-prune_thresh=-1
-prune_iter=-1
-
-feedforward=0
-ref_vid=0
-bgmodel="no" #"plane"
-depth_occ=0
-depth_vid="0"
-trgt_id=0
-manual_depth_view=1
-init_view_num=3
-pre_d_est="${nrCheckpoint}/MVSNet/model_000014.ckpt"
-manual_std_depth=0.0
-depth_conf_thresh=0.8
-geo_cnsst_num=0
 edge_filter=10 # pixels crop out at image edge
 
-appr_feature_str0="imgfeat_0_0123 dir_0 point_conf"
 point_conf_mode="1" # 0 for only at features, 1 for multi at weight
 point_dir_mode="1" # 0 for only at features, 1 for color branch
 point_color_mode="1" # 0 for only at features, 1 for color branch
-default_conf=-1
 
 agg_feat_xyz_mode="None"
 agg_alpha_xyz_mode="None"
@@ -58,6 +34,7 @@ SR=24
 K=8
 P=26
 NN=2
+
 
 act_type="LeakyReLU"
 
@@ -117,35 +94,8 @@ gpu_ids='2'
 checkpoints_dir="${nrCheckpoint}/scannet/"
 resume_dir="${nrCheckpoint}/init/dtu_dgt_d012_img0123_conf_agg2_32_dirclr20"
 
-save_iter_freq=10000
-save_point_freq=10000 #301840 #1
-maximum_step=200000 #500000 #250000 #800000
-
-niter=10000 #1000000
-niter_decay=10000 #250000
-n_threads=2
-
-train_and_test=0 #1
-test_num=10
-test_freq=10000 #  #100 #1200 #1200 #30184 #30184 #50000
-print_freq=40
-test_num_step=50
-
-prob_freq=10000 #10001
-prob_num_step=100
-prob_kernel_size=" 3 3 3 1 1 1 "
-prob_tiers=" 40000 120000 "
-prob_mode=0 # 0, n, 1 t, 10 t&n
-prob_thresh=0.7
-prob_mul=0.4
-
-zero_epsilon=1e-3
-
+test_num_step=1
 visual_items='coarse_raycolor gt_image '
-zero_one_loss_items='conf_coefficient' #regularize background to be either 0 or 1
-zero_one_loss_weights=" 0.0001 "
-sparse_loss_weight=0
-
 color_loss_weights=" 1.0 0.0 0.0 "
 color_loss_items='ray_masked_coarse_raycolor ray_miss_coarse_raycolor coarse_raycolor'
 test_color_loss_items='coarse_raycolor ray_miss_coarse_raycolor ray_masked_coarse_raycolor'
@@ -157,11 +107,7 @@ split="train"
 
 cd run
 
-for i in $(seq 1 $prob_freq $maximum_step)
-
-do
-
-python3 train_ft.py \
+python3 test_ft.py \
         --name $name \
         --scan $scan \
         --data_root $data_root \
@@ -175,27 +121,11 @@ python3 train_ft.py \
         --random_sample $random_sample \
         --random_sample_size $random_sample_size \
         --batch_size $batch_size \
-        --maximum_step $maximum_step \
-        --plr $plr \
-        --lr $lr \
-        --lr_policy $lr_policy \
-        --lr_decay_iters $lr_decay_iters \
-        --lr_decay_exp $lr_decay_exp \
         --gpu_ids $gpu_ids \
         --checkpoints_dir $checkpoints_dir \
-        --save_iter_freq $save_iter_freq \
-        --niter $niter \
-        --niter_decay $niter_decay \
-        --n_threads $n_threads \
         --pin_data_in_memory $pin_data_in_memory \
-        --train_and_test $train_and_test \
-        --test_num $test_num \
-        --test_freq $test_freq \
         --test_num_step $test_num_step \
         --test_color_loss_items $test_color_loss_items \
-        --prob_freq $prob_freq \
-        --prob_num_step $prob_num_step \
-        --print_freq $print_freq \
         --bg_color $bg_color \
         --split $split \
         --which_ray_generation $which_ray_generation \
@@ -203,7 +133,6 @@ python3 train_ft.py \
         --far_plane $far_plane \
         --dir_norm $dir_norm \
         --which_tonemap_func $which_tonemap_func \
-        --load_points $load_points \
         --resume_dir $resume_dir \
         --resume_iter $resume_iter \
         --feature_init_method $feature_init_method \
@@ -220,7 +149,6 @@ python3 train_ft.py \
         --agg_feat_xyz_mode $agg_feat_xyz_mode \
         --agg_alpha_xyz_mode $agg_alpha_xyz_mode \
         --agg_color_xyz_mode $agg_color_xyz_mode  \
-        --save_point_freq $save_point_freq  \
         --raydist_mode_unit $raydist_mode_unit  \
         --agg_dist_pers $agg_dist_pers \
         --agg_intrp_order $agg_intrp_order \
@@ -240,47 +168,17 @@ python3 train_ft.py \
         --apply_pnt_mask $apply_pnt_mask \
         --point_features_dim $point_features_dim \
         --color_loss_items $color_loss_items \
-        --feedforward $feedforward \
-        --trgt_id $trgt_id \
-        --depth_vid $depth_vid \
-        --ref_vid $ref_vid \
-        --manual_depth_view $manual_depth_view \
-        --pre_d_est $pre_d_est \
-        --depth_occ $depth_occ \
-        --manual_std_depth $manual_std_depth \
         --visual_items $visual_items \
-        --appr_feature_str0 $appr_feature_str0 \
-        --init_view_num $init_view_num \
-        --feat_grad $feat_grad \
-        --conf_grad $conf_grad \
-        --dir_grad $dir_grad \
-        --color_grad $color_grad \
-        --depth_conf_thresh $depth_conf_thresh \
-        --bgmodel $bgmodel \
-        --vox_res $vox_res \
         --act_type $act_type \
-        --geo_cnsst_num $geo_cnsst_num \
         --point_conf_mode $point_conf_mode \
         --point_dir_mode $point_dir_mode \
         --point_color_mode $point_color_mode \
         --normview $normview \
-        --prune_thresh $prune_thresh \
-        --prune_iter $prune_iter \
-        --sparse_loss_weight $sparse_loss_weight \
-        --zero_one_loss_items $zero_one_loss_items \
-        --zero_one_loss_weights $zero_one_loss_weights \
-        --default_conf $default_conf \
         --edge_filter $edge_filter \
         --vsize $vsize \
         --wcoord_query $wcoord_query \
         --ranges $ranges \
         --z_depth_dim $z_depth_dim \
         --max_o $max_o \
-        --prob_thresh $prob_thresh \
-        --prob_mul $prob_mul \
-        --prob_kernel_size $prob_kernel_size \
-        --prob_tiers $prob_tiers \
         --query_size $query_size \
         --debug
-
-done
